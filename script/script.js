d929e8580cd132a8ea5c533dd3d1fa1af8531a97
Abdo Khattab <mr.spykhattab@gmail.com>
2023-10-14 15:09:58 +0300

    Added bookmark box

--- script/script.js
+++ script/script.js
@@ -1,8 +1,22 @@
 const massageIcon = document.querySelector(".user__massage-icon");
 const massageBox = document.querySelector(".user__massage-box");
 const closeMassageBox = document.querySelector(".close-btn");
+
+const bookmarkIcon = document.querySelector(".user__bookmark-icon");
+const bookmarkBox = document.querySelector(".user__bookmark-box");
+const closeBookmarkBox = document.querySelector(".close-btn-bookmark");
+
 massageIcon.addEventListener("click", () => {
+  if (bookmarkBox.classList.contains("active-box")) {
+    bookmarkBox.classList.toggle("active-box");
+  }
   massageBox.classList.toggle("active-box");
+});
+bookmarkIcon.addEventListener("click", () => {
+  if (massageBox.classList.contains("active-box")) {
+    massageBox.classList.toggle("active-box");
+  }
+  bookmarkBox.classList.toggle("active-box");
 });
 closeMassageBox.addEventListener("click", () => {
   massageBox.classList.toggle("active-box");
